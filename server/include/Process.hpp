e3ac1572b3d2b50cbf3679b894876d26c14f2d2d
phattruongPO344 <96862579+phattruongPO344@users.noreply.github.com>
2025-08-15 23:15:03 +0700

    [Add] send respond windowcommand and fille  and process

--- server/include/Process.hpp
+++ server/include/Process.hpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
 
 #include "RequestHandler.hpp"
 #include "Response.hpp"
@@ -10,23 +11,19 @@
 struct ProcessInfo {
     DWORD PID; //ProcessID
     char Name[260];
-    std::wstring FullPath;
     DWORD ParentPID;
-    DWORD ThreadCount;
     SIZE_T MemoryUsage;
     ULONGLONG CPUTimeUser;
-    ULONGLONG CPUTimeKernel;
-    DWORD PriorityClass;
-    DWORD SessionID;
 };
 
 class Process: public FeatureHandler{
-    constexpr static int REQUEST_KEY = 0x01;
+    constexpr static int REQUEST_KEY = 0x00;
 public:
     static bool TerminateProcessByID(const int& PID);
     static bool OpenFileByPath(const std::string& FilePath);
     static std::vector<ProcessInfo> ListProcess();
-    static void PrintProcessList(const std::vector<ProcessInfo>& ProcessList);
+    static std::string ProcessListToMessage(std::vector<ProcessInfo>& ProcessList);
+
 
     void HandleRequest(SOCKET client_socket, const PacketHeader &header) override;
 };
